a255b48276733bcfa4675992299022da5b2a41f7
Ana Caroline Mota Diniz <125372805+HachiCna@users.noreply.github.com>
2023-04-05 22:32:16 -0300

    Add files via upload

---
+++ exercbd01.sql
@@ -0,0 +1,9 @@
+--Criação do Banco de Dados
+CREATE DATABASE ExercBD02
+ 
+ use ExercBD02 --Escolhendo o Banco criado
+ --Criando tabela
+ CREATE TABLE Departamento (
+  CodFunc int, --Criando campo CodFunc
+  NomeDepto varchar(50) --Criando campo NomeDepto
+  );
